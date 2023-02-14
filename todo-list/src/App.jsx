f63285101e715b5190e6882c23044a68f1f65940
Jaytee-Thomas <jt.aws.cloud@protonmail.com>
2023-02-14 09:47:51 -0600

    Execise 1 project structure completed

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//this is the react class that will be rendered to the DOM
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
