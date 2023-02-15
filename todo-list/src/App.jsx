f512bd5f2f83ae7d5da6840871c6240bf138cad9
Jaytee-Thomas <jt.aws.cloud@protonmail.com>
2023-02-14 20:35:42 -0600

    Exercise: State and Props complete

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -7,12 +7,28 @@ class App extends React.Component {
       list: ["ready", "set", "GO"],
       text: ""
     };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ text: event.target.value });
+  };
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.setState((prevState) => ({
+      list: [...prevState.list, prevState.text],
+      text: ""
+    }));
   }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.text} onChange={this.handleInputChange} />
+          <button type="submit">Add</button>
+        </form>
         <ul>
           {this.state.list.map((item, index) => {
             return <li key={index}>{item}</li>;
